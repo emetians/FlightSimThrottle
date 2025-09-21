67af74b4215b2da8fd5fcc2388b6de48a7f3aa85
emetians <aliosmanemetli@gmail.com>
2025-09-21 17:36:24 +0300

    variable names updated

--- FlightSimThrottle/FlightSimThrottle.ino
+++ FlightSimThrottle/FlightSimThrottle.ino
@@ -28,20 +28,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int rightThrottleValue = analogRead(PIN_AXIS_THROTTLE_R); // max: 373
-  int leftThrottleValue = 1023 - analogRead(PIN_AXIS_THROTTLE_L); // 1023 - readed value means revering axis, max: 448
-  int spoilerValue = 1023 - analogRead(PIN_AXIS_SPOILER); // max: 645
-  bool encButtonState = digitalRead(PIN_BUTTON_ENC); // when pressed 0
-  bool encAState = digitalRead(PIN_ENC_A);
-  bool encBState = digitalRead(PIN_ENC_A);
-  bool leftSwitchState = digitalRead(PIN_SWITCH_LEFT); // when up 1
-  bool rightSwitchState = digitalRead(PIN_SWITCH_RIGHT); // when up 1
-  bool throttleButtonState = digitalRead(PIN_BUTTON_THROTTLE); // when pressed 0
+  int thRightVal = analogRead(PIN_AXIS_THROTTLE_R); // max: 373
+  int thLeftVal = 1023 - analogRead(PIN_AXIS_THROTTLE_L); // 1023 - readed value means revering axis, max: 448
+  int spVal = 1023 - analogRead(PIN_AXIS_SPOILER); // max: 645
+  bool butEncSt = digitalRead(PIN_BUTTON_ENC); // when pressed 0
+  bool encASt = digitalRead(PIN_ENC_A);
+  bool encBSt = digitalRead(PIN_ENC_A);
+  bool swLeftSt = digitalRead(PIN_SWITCH_LEFT); // when up 1
+  bool swRightSt = digitalRead(PIN_SWITCH_RIGHT); // when up 1
+  bool butThSt = digitalRead(PIN_BUTTON_THROTTLE); // when pressed 0
 
   char message[1000];
   sprintf(message, "RT: %d, LT: %d, SP: %d, encBt: %d, encA: %d, encB:%d, LSw: %d, RSw: %d, thBT: %d\n",
-                    rightThrottleValue, leftThrottleValue, spoilerValue, encButtonState, encAState,
-                    encBState, leftSwitchState, rightSwitchState, throttleButtonState);
+                    thRightVal, thLeftVal, spVal, butEncSt, encASt,
+                    encBSt, swLeftSt, swRightSt, butThSt);
   Serial.print(message);
   delay(500);
 }
